138cae6b183e3be0014449c2be03626c96b3b299
Jinto Jose <jintoppy@gmail.com>
2017-07-11 07:31:20 +0530

    fixing issue in app reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,9 +1,20 @@
 import {combineReducers} from '@ngrx/store';
+import { ActionReducer } from '@ngrx/store';
+import { compose } from '@ngrx/core/compose';
 import {cartreducer} from './cart.reducer';
 import {catalogreducer} from './catalog.reducer';
+import { environment } from '../../environments/environment';
+import { storeFreeze } from 'ngrx-store-freeze';
 
-export const AppReducer = combineReducers({
+
+const reducers = {
     catalog: catalogreducer,
     cart: cartreducer
-});
+};
 
+const productionReducer: ActionReducer<object> = combineReducers(reducers);
+
+
+export function AppReducer(state: any, action: any) {
+  return productionReducer(state, action);
+};
